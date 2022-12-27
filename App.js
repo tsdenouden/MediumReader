72d90fb3cd275f771e4962dfb7fc421cd13e6121
Tristan Shawn den Ouden <trisdo687@gmail.com>
2022-12-27 01:05:52 +0100

    articles

--- App.js
+++ App.js
@@ -1,49 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { useState, useEffect } from 'react';
-import XMLParser from 'react-xml-parser';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import MediumFeed from './src/screens/MediumFeed';
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch('https://medium.com/feed/@thespacesmag')
-    .then(response => response.text())
-    .then(feed => {
-      // xml feed to json
-      let xmlFeed = new XMLParser().parseFromString(feed);
-      xmlFeed = xmlFeed.children[0].children;
-
-      // get posts from feed
-      let newPosts = [];
-      xmlFeed
-        .filter(element => element.name === 'item')
-        .forEach(post => {
-          let attributes = post.children;
-          let newPost = {
-            title: attributes[0]?.value,
-            content: attributes[attributes.length-1]?.value
-          };
-          newPosts.push(newPost);
-        })
-      setPosts(newPosts);
-    })
-    .catch(err => {
-      console.error(err);
-    })
-  }, []);
-
   return (
-    <ScrollView>
-      <Text>React Native App!</Text>
-      {posts.map((post, index) => 
-        <View key={index}>
-          <Text>{post.title}</Text>
-        </View>
-      )}
+    <SafeAreaView style={styles.app}>
+      <MediumFeed />
       <StatusBar style="auto" />
-    </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    display: 'flex',
+    alignContent: 'center',
+    paddingTop: 15
+  }
+})
 
 export default App;
---
+++ src/screens/MediumFeed.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import FetchPosts from '../services/FetchPosts';
+import Post from '../components/Post';
+
+const MediumFeed = () => {
+    const [posts, setPosts] = useState([]);
+    const [author, setAuthor] = useState('thespacesmag');
+
+    useEffect(() => {
+        // fetch posts from author
+        FetchPosts(author)
+            .then(posts => setPosts(posts))
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <ScrollView>
+            <Text style={styles.header}>
+                @{author}
+            </Text>
+            {posts.map((post, index) => 
+                <Post 
+                    title={post.title} 
+                    content={post.content} 
+                    key={index} 
+                />
+            )}
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 30,
+        fontFamily: 'monospace',
+    }
+});
+
+export default MediumFeed;
